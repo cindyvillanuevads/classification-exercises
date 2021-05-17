88a178dc20edddcceb61beec2dc6e76d9c50d2a5
Cindy Villanueva <cindy.villanueva.ds@gmail.com>
2021-05-17 17:21:56 -0500

    add titanic prepare function

--- prepare.py
+++ prepare.py
@@ -1,10 +1,22 @@
-import numpy as np
-import pandas as pd
+
 import seaborn as sns
 import os
 from pydataset import data
 from scipy import stats
+import pandas as pd
+import numpy as np
 
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder, MinMaxScaler
+
+# ignore warnings
+import warnings
+warnings.filterwarnings("ignore")
+
+from acquire import get_titanic_data, get_iris_data
+
+# ************************************ IRIS DATA ***********************************
 
 def prep_iris(df):
     '''
@@ -22,4 +34,60 @@ def prep_iris(df):
     df = pd.concat([df, dummy_df], axis =1)
     return df
     
+# ************************************ TITANIC DATA***********************************
+def split_data(df):
+    '''
+    take in a DataFrame and return train, validate, and test DataFrames; stratify on survived.
+    return train, validate, test DataFrames.
+    '''
+    train_validate, test = train_test_split(df, test_size=.2, random_state=123, stratify=df.survived)
+    train, validate = train_test_split(train_validate, 
+                                       test_size=.3, 
+                                       random_state=123, 
+                                       stratify=train_validate.survived)
+    return train, validate, test
+
+def impute_mode(train, validate, test, column , method):
+    '''
+    impute a choosen strategy (method) for age column into observations with missing values.
+    column:  is the column to impute or fill the missing values in and 
+    method:  is the type of strategy(media, media, most_frequent)
+    '''
+    imputer = SimpleImputer(strategy= method, missing_values= np.nan)
+    train[[column]] = imputer.fit_transform(train[[column]])
+    validate[[column]] = imputer.transform(validate[[column]])
+    test[[column]] = imputer.transform(test[[column]])
+    return train, validate, test
+
+
+def prep_titanic_data(df, column, method ,dummies):
+    '''
+    takes in a dataframe of the titanic dataset that was  acquired before and returns a cleaned dataframe
+    arguments:
+    - df: a pandas DataFrame with the expected feature names and columns
+    - column : the name of the column to fill or impute the missing values in
+    - method: type of strategy (median, mean, most_frequent) for SimpleImputer
+    - dummies: list of columns to create a dummy variable 
+    return: 
+    train, validate, test (three dataframes with the cleaning operations performed on them)
+    '''
+    #clean data
+    df = df.drop_duplicates()
+    df = df.drop(columns=['deck', 'embark_town'])
     
+    #create a dummy df
+    dummy_df = pd.get_dummies(df[dummies], dummy_na=False, drop_first=[True, True])
+    ## Concatenate the dummy_df dataframe above with the original df
+    df = pd.concat([df, dummy_df], axis=1)
+    
+    # drop the deck column
+    df = df.drop(columns= dummies)
+    #split data
+    
+    # split data into train, validate, test dfs
+    train, validate, test = split_data(df)
+
+    # impute the chosen strategy (median)  for  the selected column (age) into null values in age column
+    train, validate, test = impute_mode(train, validate, test, column , method)
+   
+    return train, validate, test
